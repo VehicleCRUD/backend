a36dada54c579e477d434a75d3f4360d254aa3e0
Edgar Andre Anco Copaja <u201724411@upc.edu.pe>
2023-10-22 10:20:36 -0500

    feat(vehicle): add vehicle mapper interface

---
+++ src/main/java/com/edgar/crudbackend/mappers/VehicleMapper.java
@@ -0,0 +1,20 @@
+package com.edgar.crudbackend.mappers;
+
+import com.edgar.crudbackend.dtos.VehicleDto;
+import com.edgar.crudbackend.entities.Vehicle;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface VehicleMapper {
+
+    Vehicle toVehicle(VehicleDto vehicleDto);
+
+    VehicleDto toVehicleDto(Vehicle vehicle);
+
+    List<VehicleDto> toVehicleDtos(List<Vehicle> vehicles);
+
+    void updateVehicle(@MappingTarget Vehicle target, Vehicle source);
+}
