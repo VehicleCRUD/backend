8691cc6421ca8b90fae9bc6eed4c45bc2afb79b7
Edgar Andre Anco Copaja <u201724411@upc.edu.pe>
2023-10-22 09:29:32 -0500

    feat(vehicle): add vehicle controller

---
+++ src/main/java/com/edgar/crudbackend/controllers/VehicleController.java
@@ -0,0 +1,22 @@
+package com.edgar.crudbackend.controllers;
+
+import com.edgar.crudbackend.dtos.VehicleDto;
+import com.edgar.crudbackend.services.VehicleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    @GetMapping("/vehicles")
+    public ResponseEntity<List<VehicleDto>> allVehicles() {
+        return ResponseEntity.ok(vehicleService.allVehicles());
+    }
+}
